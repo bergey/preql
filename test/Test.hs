34c8eaba99d435026ce716d28d0e44b312a85aa8
Daniel Bergey <bergey@teallabs.org>
2019-07-25 17:37:06 +0000

    tests for INSERT

--- test/Test.hs
+++ test/Test.hs
@@ -4,8 +4,11 @@ import Syntax
 import Printer
 import Internal (Name, mkName)
 
+import Data.List.NonEmpty (NonEmpty(..))
 import Test.Tasty
 import Test.Tasty.HUnit
+
+import qualified Data.List.NonEmpty as NE
 
 main :: IO ()
 main = defaultMain $ testGroup "crispy-broccoli" [
@@ -23,4 +26,20 @@ main = defaultMain $ testGroup "crispy-broccoli" [
                   { table = mkName "taffy"
                   , conditions = Just (Op Eq (mkName "flavor") (Lit (T"blueberry")))
                   }))
+    , testCase "INSERT, one column" $
+        assertEqual ""
+            "INSERT INTO users (email) VALUES ('bergey@teallabs.org')"
+            (fmt (QI Insert
+                 { table = mkName "users"
+                 , columns = mkName "email" :| []
+                 , values = T "bergey@teallabs.org" :| []
+                 }))
+    , testCase "INSERT, two columns" $
+        assertEqual ""
+            "INSERT INTO users (email, first_name) VALUES ('bergey@teallabs.org', 'Daniel')"
+            (fmt (QI Insert
+                 { table = mkName "users"
+                 , columns = mkName "email" :| [ mkName "first_name" ]
+                 , values = T "bergey@teallabs.org" :| [ T "Daniel" ]
+                 }))
     ]

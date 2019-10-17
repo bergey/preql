643e5482375791e407d82caccdcb80a7b426375f
Daniel Bergey <bergey@teallabs.org>
2019-10-17 00:40:36 +0000

    instance IsString Name

--- src/Internal.hs
+++ src/Internal.hs
@@ -4,10 +4,16 @@ module Internal (
     Name, mkName, getName
     ) where
 
-import           Data.Text (Text)
+import           Data.String (IsString (..))
+import           Data.Text   (Text)
+
+import qualified Data.Text   as T
 
 newtype Name = Name Text
     deriving (Show, Eq, Ord)
+
+instance IsString Name where
+    fromString = Name . T.pack
 
 -- TODO mkName should enforce valid characters in Name
 mkName :: Text -> Name

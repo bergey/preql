9683d0c89c8c8ed0150ea1e03b3714be9952d3b3
Daniel Bergey <bergey@teallabs.org>
2019-10-30 03:56:13 +0000

    update EffectClass to use TypedQuery

--- src/SqlEffect.hs
+++ src/SqlEffect.hs
@@ -4,7 +4,11 @@
 {-# LANGUAGE DefaultSignatures #-}
 -- | SQL Effect class, basically capturing some way of accessing a database.
 
-module SqlEffect where
+module SqlEffect
+    ( TypedQuery, SQL(..)
+    ) where
+
+import TypedQuery (TypedQuery(..))
 
 import Database.PostgreSQL.Simple
 
@@ -15,15 +19,21 @@ import Control.Monad.Trans.State (StateT)
 import Control.Monad.Trans.Writer (WriterT)
 import Control.Monad.Trans.Class (MonadTrans(..))
 
-class Monad m => SQL (m :: * -> *) where
-    runQuery :: (ToRow p, FromRow r) => Query -> p -> m [r]
-    runQuery_ :: FromRow r => Query -> m [r]
+import qualified TypedQuery as TQ
 
-    default runQuery :: (MonadTrans t, SQL m', m ~ t m', ToRow p, FromRow r) => Query -> p -> m [r]
+class Monad m => SQL (m :: * -> *) where
+    runQuery :: (ToRow p, FromRow r) => TypedQuery p r -> p -> m [r]
+    runQuery_ :: FromRow r => TypedQuery p () -> m [r]
+    executeSql :: ToRow p => TypedQuery p () -> m Int64
+
+    default runQuery :: (MonadTrans t, SQL m', m ~ t m', ToRow p, FromRow r) => TypedQuery p r -> p -> m [r]
     runQuery q p = lift (runQuery q p)
 
-    default runQuery_ :: (MonadTrans t, SQL m', m ~ t m', FromRow r) => Query -> m [r]
+    default runQuery_ :: (MonadTrans t, SQL m', m ~ t m', FromRow r) => TypedQuery -> m [r]
     runQuery_ = lift . runQuery_
+
+    default executeSql :: (MonadTrans t, SQL m', m ~ t m', ToRow p) => TypedQuery p () -> p -> m Int64
+    executeSql q p = lift (executeSql q p)
 
 -- | Most larger applications will define an instance; this one is suitable to test out the library.
 instance SQL (ReaderT Connection IO) where
@@ -33,6 +43,9 @@ instance SQL (ReaderT Connection IO) where
     runQuery_ q = do
         conn <- ask
         lift $ query_ conn q
+    executeSql q p = do
+        conn <- ask
+        lift $ execute conn q p
 
 instance SQL m => SQL (ExceptT e m)
 instance SQL m => SQL (MaybeT m)

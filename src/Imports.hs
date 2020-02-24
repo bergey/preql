2a4f2d34a55d962a020d7f0563669f03c728c23a
Daniel Bergey <bergey@simspace.com>
2020-02-23 19:05:56 -0500

    common imports

---
+++ src/Imports.hs
@@ -0,0 +1,23 @@
+-- |  Common imports, so I don't need to repeat them everywhere
+
+module Imports
+    ( module X
+    , decodeUtf8With, lenientDecode
+    , Vector, Text, ByteString
+    )
+
+where
+
+import Control.Applicative as X
+import Control.Exception as X (Exception)
+import Control.Monad.IO.Class as X (liftIO, MonadIO)
+import Data.Bifunctor as X
+import Data.ByteString (ByteString)
+import Data.Functor as X
+import Data.Maybe as X (catMaybes)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8With)
+import Data.Text.Encoding.Error (lenientDecode)
+import Data.Traversable as X
+import Data.Typeable as X
+import Data.Vector (Vector)

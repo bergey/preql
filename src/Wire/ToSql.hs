3c1c971e86c55cc95c75fb6d813be2eaf7ffea0b
Daniel Bergey <bergey@simspace.com>
2020-02-23 21:27:13 -0500

    binary encoders

--- src/Wire/ToSql.hs
+++ src/Wire/ToSql.hs
@@ -2,92 +2,71 @@
 
 module Wire.ToSql where
 
-import           Data.ByteString                            (ByteString)
-import           Data.ByteString.Builder                    (Builder)
+import           Imports
+
 import           Data.Functor.Contravariant
 import           Data.Functor.Contravariant.Divisible
 import           Data.Int
-import           Database.PostgreSQL.Simple.ToField         (inQuotes)
 
-import qualified Data.ByteString                            as BS
-import qualified Data.ByteString.Builder                    as B
-import qualified Data.ByteString.Lazy                       as BSL
-import qualified Database.PostgreSQL.LibPQ                  as PQ
+import qualified Data.ByteString as BS
+-- import qualified Data.ByteString.Builder as B
+-- import qualified Data.ByteString.Lazy as BSL
+import qualified ByteString.StrictBuilder as B
+import qualified Database.PostgreSQL.LibPQ as PQ
 import qualified Database.PostgreSQL.Simple.TypeInfo.Static as OID
+import qualified PostgreSQL.Binary.Encoding as PGB
 
-data FieldEncoder a = FieldEncoder PQ.Oid (a -> ByteString)
+data FieldEncoder a = FieldEncoder PQ.Oid (a -> B.Builder)
 
 instance Contravariant FieldEncoder where
     contramap f (FieldEncoder oid enc) = FieldEncoder oid (enc . f)
 
--- TODO benchmark, consider not-list
-newtype RowEncoder a = RowEncoder [FieldEncoder a]
+runFieldEncoder :: FieldEncoder p -> p -> (PQ.Oid, ByteString)
+runFieldEncoder (FieldEncoder oid enc) p = (oid, B.builderBytes (enc p))
 
-instance Contravariant RowEncoder where
-    contramap f (RowEncoder fields) = RowEncoder (map (contramap f) fields)
+type RowEncoder a = a -> [(PQ.Oid, ByteString)]
 
-instance Divisible RowEncoder where
-    divide pair fb fc = RowEncoder (bs <> cs) where
-      RowEncoder bs = contramap (fst . pair) fb
-      RowEncoder cs = contramap (snd . pair) fc
-    conquer = RowEncoder [] -- identity, does not actually encode any fields
-
-runFieldEncoder :: p -> FieldEncoder p -> (PQ.Oid, ByteString, PQ.Format)
-runFieldEncoder p (FieldEncoder oid enc) = (oid, enc p, PQ.Text)
-
--- TODO think harder about Maybe
 runEncoder :: RowEncoder p -> p -> [Maybe (PQ.Oid, ByteString, PQ.Format)]
-runEncoder (RowEncoder fields) p = map (Just . runFieldEncoder p) fields
+runEncoder fields p = fields p <&> \(oid, bs) -> Just (oid, bs, PQ.Binary)
+
+oneField :: FieldEncoder a -> RowEncoder a
+oneField enc = \p -> [runFieldEncoder enc p]
+
+class ToSqlField a where
+    toSqlField :: FieldEncoder a
 
 class ToSql a where
     toSql :: RowEncoder a
 
+instance ToSqlField Int32 where
+    toSqlField = FieldEncoder OID.int4Oid PGB.int4_int32
+instance ToSql Int32 where toSql = oneField toSqlField
+
+instance ToSqlField Int64 where
+    toSqlField = FieldEncoder OID.int8Oid PGB.int8_int64
+instance ToSql Int64 where toSql = oneField toSqlField
+
+instance ToSqlField Float where
+    toSqlField = FieldEncoder OID.float4Oid PGB.float4
+instance ToSql Float where toSql = oneField toSqlField
+
+instance ToSqlField Double where
+    toSqlField = FieldEncoder OID.float8Oid PGB.float8
+instance ToSql Double where toSql = oneField toSqlField
+
+instance ToSqlField Text where
+    toSqlField = FieldEncoder OID.textOid PGB.text_strict
+instance ToSql Text where toSql = oneField toSqlField
+
 instance ToSql () where
-    toSql = conquer
+    toSql () = []
 
-instance (ToSql a, ToSql b) => ToSql (a, b) where
-    toSql = divided toSql toSql
-instance (ToSql a, ToSql b, ToSql c) => ToSql (a, b, c) where
-    toSql = divide (\(a, b, c) -> (a, (b, c))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d) => ToSql (a, b, c, d) where
-    toSql = divide (\(a, b, c, d) -> (a, (b, c, d))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e) => ToSql (a, b, c, d, e) where
-    toSql = divide (\(a, b, c, d, e) -> (a, (b, c, d, e))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e, ToSql f) => ToSql (a, b, c, d, e, f) where
-    toSql = divide (\(a, b, c, d, e, f) -> (a, (b, c, d, e, f))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e, ToSql f, ToSql g) => ToSql (a, b, c, d, e, f, g) where
-    toSql = divide (\(a, b, c, d, e, f, g) -> (a, (b, c, d, e, f, g))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e, ToSql f, ToSql g, ToSql h) => ToSql (a, b, c, d, e, f, g, h) where
-    toSql = divide (\(a, b, c, d, e, f, g, h) -> (a, (b, c, d, e, f, g, h))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e, ToSql f, ToSql g, ToSql h, ToSql i) => ToSql (a, b, c, d, e, f, g, h, i) where
-    toSql = divide (\(a, b, c, d, e, f, g, h, i) -> (a, (b, c, d, e, f, g, h, i))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e, ToSql f, ToSql g, ToSql h, ToSql i, ToSql j) => ToSql (a, b, c, d, e, f, g, h, i, j) where
-    toSql = divide (\(a, b, c, d, e, f, g, h, i, j) -> (a, (b, c, d, e, f, g, h, i, j))) toSql toSql
-instance (ToSql a, ToSql b, ToSql c, ToSql d, ToSql e, ToSql f, ToSql g, ToSql h, ToSql i, ToSql j, ToSql k) => ToSql (a, b, c, d, e, f, g, h, i, j, k) where
-    toSql = divide (\(a, b, c, d, e, f, g, h, i, j, k) -> (a, (b, c, d, e, f, g, h, i, j, k))) toSql toSql
-
-field :: PQ.Oid -> (a -> ByteString) -> RowEncoder a
-field oid enc = RowEncoder [FieldEncoder oid enc]
-
-builder :: PQ.Oid -> (a -> Builder) -> RowEncoder a
-builder oid enc = field oid (BSL.toStrict . B.toLazyByteString . enc)
-
-instance ToSql Int32 where
-    toSql = builder OID.int4Oid B.int32Dec -- Text format
-
-instance ToSql Int64 where
-    toSql = builder OID.int8Oid B.int64Dec
-
-instance ToSql Float where
-    toSql = builder OID.float4Oid (\v ->
-        if isNaN v || isInfinite v
-        then inQuotes (B.floatDec v)
-        else B.floatDec v
-        )
-
-instance ToSql Double where
-    toSql = builder OID.float8Oid (\v ->
-        if isNaN v || isInfinite v
-        then inQuotes (B.doubleDec v)
-        else B.doubleDec v
-        )
+instance (ToSqlField a, ToSqlField b) => ToSql (a, b) where
+    toSql (a, b) = [runFieldEncoder toSqlField a, runFieldEncoder toSqlField b]
+instance (ToSqlField a, ToSqlField b, ToSqlField c) => ToSql (a, b, c) where
+    toSql (a, b, c) =
+        [runFieldEncoder toSqlField a, runFieldEncoder toSqlField b, runFieldEncoder toSqlField c]
+instance (ToSqlField a, ToSqlField b, ToSqlField c, ToSqlField d) => ToSql (a, b, c, d) where
+    toSql (a, b, c, d) =
+        [runFieldEncoder toSqlField a, runFieldEncoder toSqlField b, runFieldEncoder toSqlField c
+        , runFieldEncoder toSqlField d]

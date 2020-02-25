1b1c959611b8e7b51bef3acdff1900eb6ea56267
Daniel Bergey <bergey@simspace.com>
2020-02-25 10:41:25 -0500

    rename Effect, make it use Wire

--- src/Preql.hs
+++ src/Preql.hs
@@ -1,5 +1,5 @@
 module Preql (module X) where
 
 import Preql.Wire as X
-import Preql.TH as X (aritySql)
--- import Preql.SqlEffect as X
+-- import Preql.TH as X (aritySql)
+import Preql.Effect as X
--- src/Preql/Effect.hs
+++ src/Preql/Effect.hs
@@ -3,53 +3,52 @@
 {-# LANGUAGE DefaultSignatures #-}
 -- | SQL Effect class, basically capturing some way of accessing a database.
 
-module Preql.SqlEffect
+module Preql.Effect
     ( Query, SQL(..)
     ) where
 
-import            Preql.Untyped.Query (Query(..))
+import           Preql.Wire
 
-import           Data.Int (Int64)
-
-import           Database.PostgreSQL.Simple (Connection, FromRow,  ToRow)
-
+import           Control.Exception (throwIO)
+import           Control.Monad.Trans.Class (MonadTrans(..))
 import           Control.Monad.Trans.Except (ExceptT)
 import           Control.Monad.Trans.Maybe (MaybeT)
 import           Control.Monad.Trans.Reader (ReaderT, ask)
 import           Control.Monad.Trans.State (StateT)
 import           Control.Monad.Trans.Writer (WriterT)
-import           Control.Monad.Trans.Class (MonadTrans(..))
+import           Data.Vector (Vector)
+import           Database.PostgreSQL.LibPQ (Connection)
 
-import qualified Preql.Untyped.Query as TQ
+import qualified Preql.Wire.Query as W
 
 -- | An Effect class for running SQL queries.  You can think of this
 -- as a context specifying a particular Postgres connection (or connection
 -- pool).
 class Monad m => SQL (m :: * -> *) where
-    query :: (ToRow p, FromRow r) => (Query p r , p) -> m [r]
-    query_ :: FromRow r => Query () r -> m [r]
-    execute :: ToRow p => (Query p (), p) -> m Int64
+    query :: (ToSql p, FromSql r) => (Query, p) -> m (Vector r)
+    query_ :: FromSql r => Query -> m (Vector r)
+    execute :: ToSql p => (Query, p) -> m ()
 
-    default query :: (MonadTrans t, SQL m', m ~ t m', ToRow p, FromRow r) => (Query p r, p) -> m [r]
+    default query :: (MonadTrans t, SQL m', m ~ t m', ToSql p, FromSql r) => (Query, p) -> m (Vector r)
     query qp = lift (query qp)
 
-    default query_ :: (MonadTrans t, SQL m', m ~ t m', FromRow r) => Query () r -> m [r]
+    default query_ :: (MonadTrans t, SQL m', m ~ t m', FromSql r) => Query -> m (Vector r)
     query_ = lift . query_
 
-    default execute :: (MonadTrans t, SQL m', m ~ t m', ToRow p) => (Query p (), p) -> m Int64
+    default execute :: (MonadTrans t, SQL m', m ~ t m', ToSql p) => (Query, p) -> m ()
     execute qp = lift (execute qp)
 
 -- | Most larger applications will define an instance; this one is suitable to test out the library.
 instance SQL (ReaderT Connection IO) where
-    query qp = do
+    query (q, p) = do
         conn <- ask
-        lift $ TQ.query conn qp
+        lift (either throwIO pure =<< W.runQuery conn q p)
     query_ q = do
         conn <- ask
-        lift $ TQ.query_ conn q
-    execute qp = do
+        lift (either throwIO pure =<< W.runQuery conn q ())
+    execute (q, p) = do
         conn <- ask
-        lift $ TQ.execute conn qp
+        lift (either throwIO pure =<< W.runQuery_ conn q p)
 
 instance SQL m => SQL (ExceptT e m)
 instance SQL m => SQL (MaybeT m)

cb9bfb3eab90675366ef24f08579541a33a60005
Daniel Bergey <bergey@simspace.com>
2020-03-21 16:06:22 -0400

    run each test case in a transaction; rollback

--- test/Test/Wire.hs
+++ test/Test/Wire.hs
@@ -9,7 +9,7 @@ import Preql.Effect
 import Preql.QuasiQuoter.Raw.TH (sql)
 import Preql.Wire
 
-import Control.Exception (Exception, throwIO)
+import Control.Exception (Exception, bracket_, throwIO)
 import Control.Monad
 import Data.ByteString (ByteString)
 import Data.Either
@@ -33,6 +33,8 @@ import qualified Preql.Wire.Query as W
 wire :: TestTree
 wire = withResource initDB PQ.finish $ \db -> testGroup "wire" $
     let
+        inTransaction desc body = testCase desc $
+            bracket_ (query_ "BEGIN TRANSACTION" ()) (query_ "ROLLBACK" ()) body
         query :: (ToSql p, FromSql r) => Query -> p -> IO (Either W.QueryError (Vector r))
         query q p = db >>= \conn -> W.query conn q p
         query_ :: (ToSql p) => Query -> p -> IO ()
@@ -40,99 +42,99 @@ wire = withResource initDB PQ.finish $ \db -> testGroup "wire" $
         assertQuery :: (FromSql r, Eq r, Show r) => Vector r -> Query -> IO ()
         assertQuery expected q = assertEqual "" (Right expected) =<< query q () in
         [ testGroup "decoders"
-          [ testCase "decode True" $
+          [ inTransaction "decode True" $
             assertQuery [True] "SELECT true"
-          , testCase "decode False" $
+          , inTransaction "decode False" $
               assertQuery [False] "SELECT false"
-          , testCase "decode Int64 literal" $
+          , inTransaction "decode Int64 literal" $
               assertQuery [2^32 :: Int64] "SELECT (2^32)::int8"
-          , testCase "decode Int32 literal" $
+          , inTransaction "decode Int32 literal" $
               assertQuery [2^16 :: Int32] "SELECT (2^16)::int4"
-          , testCase "decode Int16 literal" $
+          , inTransaction "decode Int16 literal" $
               assertQuery [2^8 :: Int16] "SELECT (2^8)::int2"
-          , testCase "decode Float literal" $
+          , inTransaction "decode Float literal" $
               assertQuery [2**32 :: Float] "SELECT (2^32)::float4"
-          , testCase "decode Double literal" $
+          , inTransaction "decode Double literal" $
               assertQuery [2**32 :: Double] "SELECT (2^32)::float8"
-          -- , testCase "decode Char literal" $
+          -- , inTransaction "decode Char literal" $
           --     assertQuery ['x'] "SELECT 'x'::char"
-          , testCase "decode String literal" $
+          , inTransaction "decode String literal" $
               assertQuery ["foo" :: String] "SELECT 'foo'::text"
-          , testCase "decode Text literal" $
+          , inTransaction "decode Text literal" $
               assertQuery ["foo" :: Text] "SELECT 'foo'::text"
-          , testCase "decode lazy Text literal" $
+          , inTransaction "decode lazy Text literal" $
               assertQuery ["foo" :: TL.Text] "SELECT 'foo'::text"
-          , testCase "decode byte array literal" $
+          , inTransaction "decode byte array literal" $
               assertQuery ["foo" :: ByteString] "SELECT 'foo'::bytea"
-          , testCase "decode UTCTime literal" $ do
+          , inTransaction "decode UTCTime literal" $ do
                   time <- iso8601ParseM "2020-03-19T21:43:00Z"
                   assertQuery [time :: UTCTime] "SELECT '2020-03-19T21:43:00Z'::timestamptz"
-          , testCase "decode Day literal" $ do
+          , inTransaction "decode Day literal" $ do
                   time <- iso8601ParseM "2020-03-19"
                   assertQuery [time :: Day] "SELECT '2020-03-19'::date"
-          , testCase "decode time literal" $ do
+          , inTransaction "decode time literal" $ do
                   time <- iso8601ParseM "21:43:00"
                   assertQuery [time :: TimeOfDay] "SELECT '21:43:00Z'::time"
-          , testCase "decode TimeTZ literal" $ do
+          , inTransaction "decode TimeTZ literal" $ do
                   time <- iso8601ParseM "21:43:00"
                   zone <- iso8601ParseM "+05:00"
                   assertQuery [TimeTZ time zone] "SELECT '21:43:00+05:00'::timetz"
-          , testCase "decode UUID literal" $
+          , inTransaction "decode UUID literal" $
               assertQuery [UUID.fromWords 0x01234567 0x89abcdef 0x01234567 0x89abcdef] "SELECT '01234567-89ab-cdef-0123-456789abcdef'::uuid"
           ]
         , testGroup "encoders"
-            [ testCase "encode True" $ do
+            [ inTransaction "encode True" $ do
                 query_ "INSERT INTO encoder_tests (b) VALUES ($1)" True
                 assertQuery [True] "SELECT b FROM encoder_tests WHERE b is not null"
-            , testCase "encode Int64" $ do
+            , inTransaction "encode Int64" $ do
                 let v = 12345 :: Int64
                 query_ "INSERT INTO encoder_tests (i64) VALUES ($1)" v
                 assertQuery [v] "SELECT i64 FROM encoder_tests WHERE i64 is not null"
-            , testCase "encode Int32" $ do
+            , inTransaction "encode Int32" $ do
                 let v = 12345 :: Int32
                 query_ "INSERT INTO encoder_tests (i32) VALUES ($1)" v
                 assertQuery [v] "SELECT i32 FROM encoder_tests WHERE i32 is not null"
-            , testCase "encode Int16" $ do
+            , inTransaction "encode Int16" $ do
                 let v = 12345 :: Int16
                 query_ "INSERT INTO encoder_tests (i16) VALUES ($1)" v
                 assertQuery [v] "SELECT i16 FROM encoder_tests WHERE i16 is not null"
-            , testCase "encode Float" $ do
+            , inTransaction "encode Float" $ do
                 let v = 12345.678 :: Float
                 query_ "INSERT INTO encoder_tests (f) VALUES ($1)" v
                 assertQuery [v] "SELECT f FROM encoder_tests WHERE f is not null"
-            , testCase "encode Double" $ do
+            , inTransaction "encode Double" $ do
                 let v = 12345.678 :: Double
                 query_ "INSERT INTO encoder_tests (d) VALUES ($1)" v
                 assertQuery [v] "SELECT d FROM encoder_tests WHERE d is not null"
-            , testCase "encode strict Text" $ do
+            , inTransaction "encode strict Text" $ do
                 let v = "foo" :: Text
                 query_ "INSERT INTO encoder_tests (t) VALUES ($1)" v
                 assertQuery [v] "SELECT t FROM encoder_tests WHERE t is not null"
-            , testCase "encode lazy Text" $ do
+            , inTransaction "encode lazy Text" $ do
                 let v = "foo" :: TL.Text
                 query_ "INSERT INTO encoder_tests (t) VALUES ($1)" v
                 assertQuery [v] "SELECT t FROM encoder_tests WHERE t is not null"
-            , testCase "encode String" $ do
+            , inTransaction "encode String" $ do
                 let v = "foo" :: String
                 query_ "INSERT INTO encoder_tests (t) VALUES ($1)" v
                 assertQuery [v] "SELECT t FROM encoder_tests WHERE t is not null"
             ]
         , testGroup "parameters & expressions"
-            [ testCase "schema type mismatch causes runtime error" $
+            [ inTransaction "schema type mismatch causes runtime error" $
                 assertBool "" . isLeft =<< query @() @Int32 "SELECT 2.5" ()
-            , testCase "ignore later columns" $
+            , inTransaction "ignore later columns" $
                 assertEqual "" (Right [2]) =<< query @() @Int32 "SELECT 2, 3" ()
-            , testCase "parse a pair" $
+            , inTransaction "parse a pair" $
                 assertEqual "" (Right [(2, 3)]) =<< query @() @(Int32, Int32) "SELECT 2, 3" ()
-            , testCase "add Int32 parameters" $
+            , inTransaction "add Int32 parameters" $
                 assertEqual "" (Right [3]) =<< query @(Int32, Int32) @Int32 "SELECT $1 + $2" (1, 2)
-            , testCase "add Int64 parameters" $
+            , inTransaction "add Int64 parameters" $
                 assertEqual "" (Right [3]) =<< query @(Int64, Int64) @Int64 "SELECT $1 + $2" (1, 2)
-            , testCase "add Float32 parameters" $
+            , inTransaction "add Float32 parameters" $
                 assertEqual "" (Right [3]) =<< query @(Float, Float) @Float "SELECT $1 + $2" (1, 2)
-            , testCase "add Float64 parameters" $
+            , inTransaction "add Float64 parameters" $
                 assertEqual "" (Right [3]) =<< query @(Double, Double) @Double "SELECT $1 + $2" (1, 2)
-            , testCase "add 3 Int32 parameters" $
+            , inTransaction "add 3 Int32 parameters" $
                 assertEqual "" (Right [6]) =<< query @(Int32, Int32, Int32) @Int32 "SELECT $1 + $2 + $3" (1, 2, 3)
             ]
         ]

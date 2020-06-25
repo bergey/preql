bcdb7b2229afb2b3ab1ae718c087828f7d71e0b8
Daniel Bergey <bergey@simspace.com>
2020-06-25 11:21:07 +0000

    nix: build benchmark & preql-load

--- nix/preql.nix
+++ nix/preql.nix
@@ -11,7 +11,7 @@
     flags = {};
     package = {
       specVersion = "1.18";
-      identifier = { name = "preql"; version = "0.2"; };
+      identifier = { name = "preql"; version = "0.3"; };
       license = "BSD-3-Clause";
       copyright = "";
       maintainer = "bergey@teallabs.org";
@@ -52,6 +52,41 @@
           ];
         buildable = true;
         };
+      exes = {
+        "preql-load" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary-parser" or (errorHandler.buildDepError "binary-parser"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."bytestring-strict-builder" or (errorHandler.buildDepError "bytestring-strict-builder"))
+            (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."free" or (errorHandler.buildDepError "free"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."postgresql-binary" or (errorHandler.buildDepError "postgresql-binary"))
+            (hsPkgs."postgresql-libpq" or (errorHandler.buildDepError "postgresql-libpq"))
+            (hsPkgs."preql" or (errorHandler.buildDepError "preql"))
+            (hsPkgs."resource-pool" or (errorHandler.buildDepError "resource-pool"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.alex or (pkgs.buildPackages.alex or (errorHandler.buildToolDepError "alex")))
+            (hsPkgs.buildPackages.happy or (pkgs.buildPackages.happy or (errorHandler.buildToolDepError "happy")))
+            ];
+          buildable = true;
+          };
+        };
       tests = {
         "tests" = {
           depends = [
@@ -70,6 +105,39 @@
             (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
             (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
             (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.alex or (pkgs.buildPackages.alex or (errorHandler.buildToolDepError "alex")))
+            (hsPkgs.buildPackages.happy or (pkgs.buildPackages.happy or (errorHandler.buildToolDepError "happy")))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "pgbench" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary-parser" or (errorHandler.buildDepError "binary-parser"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."bytestring-strict-builder" or (errorHandler.buildDepError "bytestring-strict-builder"))
+            (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."free" or (errorHandler.buildDepError "free"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."postgresql-binary" or (errorHandler.buildDepError "postgresql-binary"))
+            (hsPkgs."postgresql-libpq" or (errorHandler.buildDepError "postgresql-libpq"))
+            (hsPkgs."preql" or (errorHandler.buildDepError "preql"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
             (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
             (hsPkgs."text" or (errorHandler.buildDepError "text"))
             (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))

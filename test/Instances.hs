d087fb3389242b7d0a7534365954140f5acd91e6
Daniel Bergey <bergey@simspace.com>
2020-09-02 11:57:00 -0400

    Arbitrary Expr can be larger if it's not part of a deep tree

--- test/Instances.hs
+++ test/Instances.hs
@@ -20,6 +20,7 @@ import qualified Data.Text           as T
 instance Arbitrary a => Arbitrary (NonEmpty a) where
     arbitrary = liftA2 (:|) arbitrary arbitrary
 
+keywords :: [String]
 keywords = ["DELETE", "SELECT", "INSERT", "FROM", "WHERE", "INTO", "VALUES", "IS", "NOT", "NULL", "ISNULL", "NOTNULL", "LIKE", "ILIKE", "AND", "OR" ]
 
 -- | These ASCII letters are only a small subset of allowed identifiers.
@@ -27,7 +28,7 @@ instance Arbitrary Name where
     -- listOf uses size; it's good to limit name length, incidental that it gets smaller deeper in the AST
     -- another sensible option would be to resize to 32, the Postgres effective name limit
     arbitrary = mkName . T.pack <$> (liftA2 (:) firstChar (listOf tailChar)
-        `suchThat` (\name -> not (map toUpper name `elem` keywords)))
+        `suchThat` (\n -> map toUpper n `notElem` keywords))
       where
         firstChar = elements $ ['a'..'z'] ++ ['A'..'Z']
         tailChar = elements $ ['a'..'z'] ++ ['A'..'Z'] ++ "0123456789_$"
@@ -37,7 +38,9 @@ instance Arbitrary Literal where
     -- TODO remaining Literal constructors
     arbitrary = oneof
         [ F <$> arbitrary
-        , T . T.pack <$> fmap getPrintableString arbitrary `suchThat` (all (/= '\''))
+        , T . T.pack <$> fmap getPrintableString arbitrary `suchThat` notElem '\''
+        , B <$> arbitrary
+        , pure Null
         ]
 
 instance Arbitrary Query where arbitrary = genericArbitraryU
@@ -69,18 +72,23 @@ instance Arbitrary Expr where
         size <- getSize
         if size <= 1
             then oneof [ Lit <$> arbitrary, CRef <$> arbitrary ]
-            else scale (`div` 2) $ oneof
-                 [ Lit <$> arbitrary, CRef <$> arbitrary
-                 , NumberedParam <$> arbitrary <*> pure []
-                 , BinOp <$> arbitrary <*> arbitrary <*> arbitrary
-                 , Unary <$> arbitrary <*> arbitrary
-                 , And <$> arbitrary <*> arbitrary
-                 , Or <$> arbitrary <*> arbitrary
-                 , Not <$> arbitrary -- scale /2 doesn't really make sense for unary constructors
-                 ]
+            else oneof (scale (`div` 2) <$>
+                        [ BinOp <$> arbitrary <*> arbitrary <*> arbitrary
+                        , And <$> arbitrary <*> arbitrary
+                        , Or <$> arbitrary <*> arbitrary
+                        , Indirection <$> arbitrary <*> arbitrary
+                        , SelectExpr <$> arbitrary <*> arbitrary
+                        ]
+                        <> map (scale pred)
+                        [ Lit <$> arbitrary, CRef <$> arbitrary
+                        , NumberedParam <$> arbitrary <*> pure []
+                        , Unary <$> arbitrary <*> arbitrary
+                        , Not <$> arbitrary
+                        ]
+                       )
     shrink expr = case expr of
         -- either subterm, or shrink one or both subterms
-        BinOp op l r -> l : r : (tail $ BinOp op <$> l : shrink l <*> r : shrink r)
+        BinOp op l r -> l : r : tail (BinOp op <$> l : shrink l <*> r : shrink r)
         Unary op e -> e : (Unary op <$> shrink e)
         _ -> [] -- remaning terms can't be shrunk
 

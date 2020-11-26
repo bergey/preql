70038ef56b8ade38e624b64f97643fc1a04377fc
Daniel Bergey <bergey@simspace.com>
2020-11-26 00:57:59 -0500

    test more branches of Expr

--- test/Test/Syntax/Generators.hs
+++ test/Test/Syntax/Generators.hs
@@ -6,6 +6,8 @@ module Test.Syntax.Generators where
 import Preql.QuasiQuoter.Syntax.Name
 import Preql.QuasiQuoter.Syntax.Syntax
 
+import Control.Applicative
+import Control.Monad
 import Data.Set (Set)
 import Data.Text (Text)
 import Hedgehog
@@ -36,15 +38,21 @@ name_ = Name <$> Gen.filter (flip Set.notMember keywords)
   (T.cons <$> Gen.lower <*>
     Gen.text (Range.linear 0 29) (Gen.frequency [(26, Gen.lower), (1, pure '_')]))
 
+haskellVarName :: Gen Text
+haskellVarName = T.cons <$> Gen.lower <*> Gen.text (Range.linear 0 29) Gen.alphaNum
+
 expr :: Gen Expr
 expr = Gen.choice -- TODO frequency
   [ litE
   , CRef <$> name_
   , NumberedParam <$> Gen.integral (Range.linear 1 20)
-  , HaskellParam <$> Gen.text (Range.linear 1 30) Gen.alphaNum -- TODO valid Haskell variable names
+  , HaskellParam <$> haskellVarName
   , Unary <$> unaryOp <*> scaleOne expr
   , BinOp <$> binOp <*> scaleHalf expr <*> scaleHalf expr
   , Indirection <$> scaleOne expr <*> Gen.nonEmpty (Range.linear 1 4) name_
+  -- TODO SelectExpr
+  , L <$> scaleOne likeE
+  , Cas <$> caseE
   -- TODO remaining constructors
   ]
 
@@ -53,6 +61,34 @@ unaryOp = Gen.enumBounded
 
 binOp :: Gen BinOp
 binOp = Gen.enumBounded
+
+likeOp :: Gen LikeOp
+likeOp = Gen.enumBounded
+
+likeE :: Gen LikeE
+likeE = do
+  op <- likeOp
+  string <- scaleHalf expr
+  likePattern <- scaleHalf expr
+  escape <- Gen.frequency
+    [(50, pure Nothing), (49, Just . Lit . T . T.singleton <$> Gen.alphaNum), (1, Just <$> scaleHalf expr)]
+  invert <- Gen.bool
+  return LikeE {..}
+
+caseE :: Gen Case
+caseE = do
+  hasImplicit <- Gen.bool
+  hasElse <- Gen.bool
+  whenSize <- Gen.integral (Range.linear 1 10)
+  let
+    oneIf x = if x then 1 else 0
+    n = 2 * whenSize + oneIf hasImplicit + oneIf hasElse
+    scale = Gen.scale (clampSize . (`div` n))
+    justIf x = if x then Just <$> scale expr else pure Nothing
+  whenClause <- replicateM (Range.unSize whenSize) (liftA2 (,) (scale expr) (scale expr))
+  implicitArg <- justIf hasImplicit
+  elseClause <- justIf hasElse
+  return Case{..}
 
 clampSize :: Size -> Size
 clampSize = clamp 0 99
